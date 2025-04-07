01a10e085e54249506227f93891f29dd60d67e6d
vincezh123 <119286801+vincezh123@users.noreply.github.com>
2025-04-07 10:35:51 +0800

    Initial commit

---
+++ mushroomClassification.py
@@ -0,0 +1,18 @@
+#%%
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+#%%
+from sklearn.svm import SVC   ##Support Vector Modifier是一种支持向量机的分类器模型。它有许多可调节的参数，
+from sklearn.linear_model import LogisticRegression ##罗辑回归
+from sklearn.ensemble import RandomForestClassifier ##随机森林
+from sklearn.preprocessing import LabelEncoder ##对数据进行编码标签eg.小狗为01小猫02 so on
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay,RocCurveDisplay,PrecisionRecallDisplay
+from sklearn.metrics import precision_score, recall_score
+
+#%%
+def main():
+    st.title("binary classification web") ##

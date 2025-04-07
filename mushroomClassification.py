ef2b881c31ab3a73dd70db539ba7e092af7fb3f8
vincezh123 <119286801+vincezh123@users.noreply.github.com>
2025-04-07 12:35:53 +0800

    Update mushroomClassification.py

--- mushroomClassification.py
+++ mushroomClassification.py
@@ -5,14 +5,14 @@ import numpy as np
 
 
 #%%
-import sklearn
-from sklearn.svm import SVC   ##Support Vector Modifier是一种支持向量机的分类器模型。它有许多可调节的参数，
-from sklearn.linear_model import LogisticRegression ##罗辑回归
-from sklearn.ensemble import RandomForestClassifier ##随机森林
-from sklearn.preprocessing import LabelEncoder ##对数据进行编码标签eg.小狗为01小猫02 so on
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import ConfusionMatrixDisplay,RocCurveDisplay,PrecisionRecallDisplay
-from sklearn.metrics import precision_score, recall_score
+# import sklearn
+# from sklearn.svm import SVC   ##Support Vector Modifier是一种支持向量机的分类器模型。它有许多可调节的参数，
+# from sklearn.linear_model import LogisticRegression ##罗辑回归
+# from sklearn.ensemble import RandomForestClassifier ##随机森林
+# from sklearn.preprocessing import LabelEncoder ##对数据进行编码标签eg.小狗为01小猫02 so on
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import ConfusionMatrixDisplay,RocCurveDisplay,PrecisionRecallDisplay
+# from sklearn.metrics import precision_score, recall_score
 
 ##remember to run pip freeze > requirements.txt in terminal to generate requirements.txt!!!
 #%%
